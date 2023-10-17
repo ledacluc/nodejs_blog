838d027a4bfc92beba91711c3977d82b9f05d1b5
ledacluc <dangchiemhd2@gmail.com>
2023-10-18 02:02:42 +0700

    18/10

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
+const { Console } = require('console')
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+    extended: true
+})); //Middleware
+
+app.use(express.json());
+
+//XMLHttpRequest, fetch, axios,
+
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', handlebars.engine({
@@ -20,13 +29,22 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 
 app.get('/news', (req, res) => { 
   res.render('news');
-})
+});
 
+app.get('/search', (req, res) => { 
+  res.render('search');
+});
+
+app.post('/search', (req, res) => { 
+
+  console.log(req.body.q);
+  res.send('');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
